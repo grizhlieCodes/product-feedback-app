c3870ec5a867ace31655dc1835617fbc8866a647
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-22 14:37:21 +0000

    Updated core CSS and Body css

--- src/lib/base/Body.svelte
+++ src/lib/base/Body.svelte
@@ -2,7 +2,9 @@
     
 </script>
 
-<div class="w-full min-h-screen relative grid bg-blue-50">
+<div class="w-full relative flex-col grid bg-blue-50 tablet:grid
+tablet:grid-cols-3 tablet:gap-[1rem] tablet:px-[4rem] tablet:py-[5.6rem]
+desktop:grid-cols-[max-content_1fr] desktop:auto-rows-max max-w-[111rem] ">
     <slot />
 </div>
 
