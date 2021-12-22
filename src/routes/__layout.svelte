7a00b14e2ddb48e4525bbd2e2d23c7f8bb0a0e30
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-22 14:39:07 +0000

    Updated layout with size observer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,17 @@
 <script>
 	import '../app.css';
-	import Body from '$lib/base/Body.svelte'
+	import Body from '$lib/base/Body.svelte';
+	import Size from '$lib/size/SizeObserver.svelte'
 </script>
+
+<Size/>
 
 <Body>
 	<slot />
 </Body>
+
+<style>
+	:global(*) {
+		font-family: 'Jost', sans-serif;
+	}
+</style>
