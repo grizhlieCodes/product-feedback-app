66fcf0b17fdab461426fef75989e16d358c2e54e
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-22 14:38:46 +0000

    Created block component.

--- src/lib/ui/Block.svelte
+++ src/lib/ui/Block.svelte
@@ -1,3 +1,9 @@
-<div class="bg-white p-[2.4rem] grid place-items-center w-full relative">
+<script>
+    export let gridColStart, gridRowStart, maxWidth
+
+</script>
+
+<div class="bg-white p-[2.4rem] grid place-items-center w-full relative
+rounded-[1rem] {gridColStart} {gridRowStart} {maxWidth}">
     <slot/>
 </div>
