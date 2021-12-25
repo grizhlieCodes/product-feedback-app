233dfcdf6de4790c11460ced681f9e06d954ffab
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-25 11:20:33 +0000

    Created roadmap 'summary' component

---
+++ src/lib/roadmap/RoadmapListItem.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let name, count
+    let bgCol = name === 'Planned' ? 'bg-orange' : name === "In-Progress" ? 'bg-violet' : 'bg-blue-500'
+</script>
+
+<li class="flex items-center gap-[1.6rem]">
+    <span class="w-[0.8rem] h-[0.8rem] block {bgCol} rounded-full"></span>
+    <span class="text-[1.6rem] text-blue-600 font-normal">{name}</span>
+    <span class="ml-auto text-[1.6rem] text-blue-600 font-bold">{count}</span>
+</li>
---
+++ src/lib/roadmap/RoadmapSummary.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let gridColStart = '', gridRowStart = '', maxWidth = ''
+    import Block from '$lib/ui/Block.svelte';
+    let roadmapItems = [{name: 'Planned', count: 2},{name: 'In-Progress', count: 3},{name: 'Live', count: 1},]
+    import RoadmapListItem from '$lib/roadmap/RoadmapListItem.svelte';
+
+</script>
+
+<Block {gridColStart} {gridRowStart} {maxWidth}>
+    <div class="w-full flex flex-col gap-[2.4rem]">
+        <div class="flex justify-between items-center">
+            <span class="text-[1.8rem] tracking-[-0.025rem] font-bold text-blue-700">Roadmap</span>
+            <a href="/" class="font-semibold text-[1.3rem] text-blue-500 underline">View</a>
+        </div>
+        <ul class="flex flex-col">
+            {#each roadmapItems as {name, count}, i}
+                <RoadmapListItem {name} {count} />
+            {/each}
+        </ul>
+    </div>
+</Block>
