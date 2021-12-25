b97de46d4a25bce13c871b37bddf96fd16bd4fd5
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-25 11:21:19 +0000

    Created feedbackCategories component

---
+++ src/lib/suggestions/FeedbackBadge.svelte
@@ -0,0 +1,16 @@
+<script>
+
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    export let badgeText, chosenBadge;
+    const activateBadge = () => {
+        dispatch('activateBadge', badgeText)
+    };
+</script>
+
+<button on:click={activateBadge} class="rounded-[1rem] px-[1.6rem] py-[0.5rem] grid
+place-items-center hover:cursor-pointer transition-colors
+{chosenBadge === badgeText ? 'bg-blue-500 text-white' : 'text-blue-500 bg-blue-100 hover:bg-blue-200'}">
+    <span class="text-[1.3rem] font-semibold">{badgeText}</span>
+</button>
