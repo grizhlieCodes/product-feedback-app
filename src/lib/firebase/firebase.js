fd7bfd2ef7502b461e4cd17bae65282f44bd7ac4
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-30 01:35:21 +0000

    Created firebase

---
+++ src/lib/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+let app
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyClZBCU0z9T5Fxbz8MZdw3Gmn3Godbo9dI",
+    authDomain: "fem-product-feedback-app.firebaseapp.com",
+    projectId: "fem-product-feedback-app",
+    storageBucket: "fem-product-feedback-app.appspot.com",
+    messagingSenderId: "342077400067",
+    appId: "1:342077400067:web:2d4986d9cbb6f1f04f5a36"
+};
+
+
+export const initialiseFirebase = () => {
+    app = initializeApp(firebaseConfig);
+    console.log({app, appInitialised: true})
+};
