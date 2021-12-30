a42913969cbd0223816603e304eadcec5b711d0a
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-30 01:37:53 +0000

    feedbackCategories refactor

--- src/lib/suggestions/FeedbackCategories.svelte
+++ src/lib/suggestions/FeedbackCategories.svelte
@@ -1,19 +1,14 @@
 <script>
 	export let gridColStart = '', gridRowStart = '', maxWidth = ''
 	import Block from '$lib/ui/Block.svelte';
-	import Badge from '$lib/suggestions/FeedbackBadge.svelte';
+	import FeedbackBadge from '$lib/suggestions/FeedbackBadge.svelte';
 	let badges = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature'];
-	let chosenBadge = 'All';
-	const activateBadge = (e) => {
-		let clickedBadge = e.detail;
-		chosenBadge = clickedBadge;
-	};
 </script>
 
 <Block {gridColStart} {gridRowStart} {maxWidth}>
 	<div id="badges" class="w-full h-max flex flex-wrap gap-x-[1.4rem] gap-y-[0.8rem]">
 		{#each badges as badge}
-			<Badge badgeText={badge} {chosenBadge} on:activateBadge={activateBadge} />
+			<FeedbackBadge badgeText={badge} />
 		{/each}
 	</div>
 </Block>
