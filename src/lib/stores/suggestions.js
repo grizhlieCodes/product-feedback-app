7e785b50d1c676b222e040fece881f0a122d7bd9
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-30 08:27:49 +0000

    added sort function to suggestions

--- src/lib/stores/suggestions.js
+++ src/lib/stores/suggestions.js
@@ -1,6 +1,5 @@
 import { writable, get } from 'svelte/store';
 import data from '$lib/data/data.json'
-import uiStore from '$lib/stores/ui-state.js'
 
 const suggestions = writable({ list: [...data.productRequests], count: data.productRequests.length })
 
@@ -8,11 +7,12 @@ const customSuggestions = {
     subscribe: suggestions.subscribe,
 
     filterSuggestions: (category) => {
+        // console.trace('Running FilterSuggestions: ' + category)
         if (category.toLowerCase() === 'all') {
             suggestions.update(suggestionz => {
                 suggestionz.list = [...data.productRequests]
                 suggestionz.count = data.productRequests.length
-                console.log(suggestionz)
+                // console.log(suggestionz)
                 return suggestionz
             })
         } else if (category !== 'all') {
@@ -21,7 +21,7 @@ const customSuggestions = {
             suggestions.update(suggestionz => {
                 suggestionz.list = [...temporaryList]
                 suggestionz.count = temporaryCount
-                console.log(suggestionz)
+                // console.log(suggestionz)
                 return suggestionz
             })
         }
@@ -40,21 +40,55 @@ const customSuggestions = {
         return temporaryList
     },
 
-    // Im braindead but my current thoughts are that I should rethink where all
-    // the store logic is contained. At the moment i have multiple elements
-    // controlling the store's values. 
-
-    //Where shuld the below function be fired from for example? SuggestionsList?
-    //SortMenu? I'm not sure.
     sortSuggestions: (sortValue) => {
         if(sortValue === 'most-upvotes'){
             suggestions.update(suggestionz => {
                 suggestionz.list.sort(function(a,b) {
-                    return a.upvotes - b.upvotes
-                })                
+                    return b.upvotes - a.upvotes
+                })               
                 return suggestionz
             })
         }
+        if(sortValue === 'least-upvotes'){
+            suggestions.update(suggestionz => {
+                suggestionz.list.sort(function(a,b) {
+                    return a.upvotes - b.upvotes
+                })               
+                return suggestionz
+            })
+        }
+        if(sortValue === 'most-comments'){
+            suggestions.update(suggestionz => {
+                suggestionz.list.sort(function(a,b) {
+                    let bCommentsCount = customSuggestions.getCommentsLength(b.comments)
+                    let aCommentsCount = customSuggestions.getCommentsLength(a.comments)
+                    return bCommentsCount - aCommentsCount
+                })               
+                return suggestionz
+            })
+        }
+        if(sortValue === 'least-comments'){
+            suggestions.update(suggestionz => {
+                suggestionz.list.sort(function(a,b) {
+                    let aCommentsCount = customSuggestions.getCommentsLength(a.comments)
+                    let bCommentsCount = customSuggestions.getCommentsLength(b.comments)
+                    return aCommentsCount - bCommentsCount
+                })               
+                return suggestionz
+            })
+        }
+    },
+
+    getCommentsLength: (comments) => {
+        let count = 0
+        comments.forEach(comment => {
+            if(comment.replies && comment.replies.length){
+                let replyCount = customSuggestions.getCommentsLength(comment.replies)
+                count = count + replyCount
+            }
+            count++
+        })
+        return count
     },
 
     consoleData: () => {
