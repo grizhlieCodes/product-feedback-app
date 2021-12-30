a80acb9105e08a45fb6bfcaac13f10c5e6a6d466
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-30 10:01:47 +0000

    fixed body gaps for desktop

--- src/lib/base/Body.svelte
+++ src/lib/base/Body.svelte
@@ -5,7 +5,7 @@
 <div class="w-full relative flex-col grid bg-blue-50 tablet:grid
 tablet:grid-cols-3 tablet:px-[4rem] tablet:py-[5.6rem]
 desktop:grid-cols-[max-content_1fr] desktop:auto-rows-max max-w-[111rem]
-tablet:gap-x-4 tablet:gap-y-16 desktop:gap-12">
+tablet:gap-x-4 tablet:gap-y-16 desktop:gap-y-10 desktop:gap-x-12">
     <slot />
 </div>
 
