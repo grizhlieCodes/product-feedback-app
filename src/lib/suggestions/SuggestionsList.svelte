3f6c555ed729bf7777bd333cf20b3788632a66e3
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-30 08:29:43 +0000

    removed automatic filterSugs function

--- src/lib/suggestions/SuggestionsList.svelte
+++ src/lib/suggestions/SuggestionsList.svelte
@@ -4,17 +4,19 @@
 
 	//Data
 	import uiStore from '$lib/stores/ui-state.js';
-	import suggestions from '$lib/stores/suggestions.js'
+	import suggestions from '$lib/stores/suggestions.js';
 
 	//Functions
-	$: suggestions.filterSuggestions($uiStore.category)
+	// $: suggestions.filterSuggestions($uiStore.category) //Potential issue.
 </script>
 
 <div class="">
-	{#each $suggestions.list as { title, description, upvotes, id, category, comments } (id)}
+	{#each $suggestions.list as { title, description, upvotes, id, category,
+	comments } (id)}
 		<div animate:flip={{ duration: 350 }}>
-			<p>{category}</p>
+			<p>Category: {category}</p>
+			<p>Upvotes: {upvotes}</p>
+			<p>Comments: {suggestions.getCommentsLength(comments)}</p>
 		</div>
 	{/each}
 </div>
-
