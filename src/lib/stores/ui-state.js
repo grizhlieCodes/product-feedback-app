076942b9bc0c6a835c846e458478c0d63684741d
grizhlieCodes <rafal.potasz@gmail.com>
2021-12-30 01:36:47 +0000

    added update functions to ui-state-store

--- src/lib/stores/ui-state.js
+++ src/lib/stores/ui-state.js
@@ -4,6 +4,22 @@ const uiStore = writable({category: 'all', sort: 'most-upvotes'})
 
 const customUiStore = {
     subscribe: uiStore.subscribe,
+    updateCategory: (category) => {
+        uiStore.update(store => {
+            store.category = category
+            return store
+        })
+        console.log(get(uiStore))
+    },
+    updateSort: (sort) => {
+        uiStore.update(store => {
+            store.sort = sort
+            return store
+        })
+        console.log(get(uiStore))
+    },
 }
 
 export default customUiStore
+
+
